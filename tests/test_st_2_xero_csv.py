0ebbcf7af084bbd81d61efeb11509873c27bfae9
lucas <lucas@normal.works>
2023-09-22 20:35:58 +0800

    新增metro bank的银行流水转化逻辑

---
+++ tests/test_st_2_xero_csv.py
@@ -0,0 +1,15 @@
+import os
+from bankstatement_copilot.st_2_xero_csv.metro_bank_st_to_xero_import_csv import (
+    process_st_csv,
+)
+
+# 文件夹路径
+directory = "/Users/shaoshuai.shao/Downloads/drive-download-20230922T102205Z-001/"
+
+# 遍历文件夹中的所有文件
+for filename in os.listdir(directory):
+    # 检查文件是否为 CSV 文件
+    if filename.endswith(".csv"):
+        inputpath = os.path.join(directory, filename)
+        outputpath = os.path.join(directory, "NEW_" + filename)
+        process_st_csv(inputpath, outputpath)
