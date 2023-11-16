e2147bd039126a06b3f0debb1242211b8410625d
lucas <lucas@normal.works>
2023-11-16 14:36:18 +0800

    新增CountingUp的银行流水

---
+++ bankstatement_copilot/st_2_xero_csv/countingup_bank_st_to_xero_import_csv.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+def process_st_csv(input_path: str, output_path: str):
+    # 读取 CSV 文件
+    df = pd.read_csv(
+        input_path,
+    )
+
+    # 填充缺失的数据为0
+    df["In (£)"].fillna(0, inplace=True)
+    df["Out (£)"].fillna(0, inplace=True)
+
+    # 合并'In (£)'和'Out (£)'列到新的列'Amount'
+    df["Amount"] = df["In (£)"] + df["Out (£)"]
+
+    # 去除'in'和'out'列
+    df.drop(columns=["In (£)", "Out (£)"], inplace=True)
+
+    # 将 DataFrame 写入新的 CSV 文件
+    df.to_csv(
+        output_path,
+        index=False,
+    )
