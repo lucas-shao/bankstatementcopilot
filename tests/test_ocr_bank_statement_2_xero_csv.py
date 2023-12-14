8992a01d26535b6d44005e1d8d2f1762f56cd5f7
lucas <lucas@normal.works>
2023-12-14 10:54:25 +0800

    修改直接ocr bank statement生成Xero的导入文件

--- tests/test_ocr_bank_statement_2_xero_csv.py
+++ tests/test_ocr_bank_statement_2_xero_csv.py
@@ -1,21 +1,60 @@
 import os
 import asyncio
 
+import pandas as pd
+
 from bankstatement_copilot.http.bkp_client import ocrBankStatement
 from bankstatement_copilot.http.model.ocr_request import OcrRequest
 
+bank_statement_file_url_list = [
+    "https://oss.xiaocichang.com/file/20231114/attachment%20TIDE%20JUL.pdf",
+    "https://oss.xiaocichang.com/file/20231114/Tide%20statement%202023%2011.pdf",
+]
+
+out_file_path = "/Users/shaoshuai.shao/Desktop/xero.csv"
+
+# 自定义列名
+column_names = ["Date", "Description", "Amount", "Balance"]
+
 
 async def main():
-    ocrRequest = OcrRequest()
-    ocrRequest.secret_key = (
-        "qI4dqCZaw0tkmwpMKrtAJ6NWabCsboPGb7wT1EUL4ohBaczIcdNac9su9NSOeGJuLH"
-    )
-    ocrRequest.file_path = (
-        "https://oss.xiaocichang.com/file/20231114/attachment%20TIDE%20JUL.pdf"
-    )
-    res = await ocrBankStatement(ocrRequest)
-    print(res)
-    print(res.data.openingBalance)
+    bankStatementLines = []
+    for bank_statement_file_url in bank_statement_file_url_list:
+        ocrRequest = OcrRequest()
+        ocrRequest.secret_key = (
+            "qI4dqCZaw0tkmwpMKrtAJ6NWabCsboPGb7wT1EUL4ohBaczIcdNac9su9NSOeGJuLH"
+        )
+        ocrRequest.file_path = bank_statement_file_url
+        res = await ocrBankStatement(ocrRequest)
+        print(res)
+
+        bankStatementLines.extend(res.data.bankStatementLineList)
+
+    if bankStatementLines != None and len(bankStatementLines) > 0:
+        # 创建一个空的DataFrame，并指定列名
+        df = pd.DataFrame(columns=column_names)
+
+        for bankStatementLine in bankStatementLines:
+            # 创建一个新的临时DataFrame来保存当前行的数据
+            temp_df = pd.DataFrame(
+                [
+                    {
+                        "Date": bankStatementLine.date,
+                        "Description": bankStatementLine.description,
+                        "Amount": bankStatementLine.amount,
+                        "Balance": bankStatementLine.balance,
+                    }
+                ]
+            )
+
+            # 使用concat方法合并DataFrame
+            df = pd.concat([df, temp_df], ignore_index=True)
+
+        # 将 DataFrame 写入新的 CSV 文件
+        df.to_csv(
+            out_file_path,
+            index=False,
+        )
 
 
 asyncio.run(main())
